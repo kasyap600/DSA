596fa846f63075bda178bb6402b2120f430f1f05
kasyapgovindu <tinkusai92@gmail.com>
2025-11-21 20:28:10 +0530

    Time: 38 ms (95.44%) | Memory: 46.8 MB (28.57%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,39 @@
+class Solution {
+    private boolean isPalindrome(String s){
+        int low = 0, high = s.length() - 1;
+        char[] c = s.toCharArray();
+        while (low < high) {
+            if (c[low] != c[high]) return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < n; i++) {
+            int idx = s.charAt(i) - 'a';
+            if (first[idx] == -1) first[idx] = i;
+            last[idx] = i;
+        }
+
+        int ans = 0;
+        for (int ch = 0; ch < 26; ch++) {
+            if (first[ch] != -1 && first[ch] < last[ch]) {
+                boolean[] seen = new boolean[26];
+                for (int k = first[ch] + 1; k < last[ch]; k++) {
+                    seen[s.charAt(k) - 'a'] = true;
+                }
+                for (int m = 0; m < 26; m++) {
+                    if (seen[m]) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}

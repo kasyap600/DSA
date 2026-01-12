d9d76421c4d437133b7118a4c97c2c55e58f1099
kasyapgovindu <tinkusai92@gmail.com>
2026-01-12 19:07:39 +0530

    Time: 10 ms (16.19%) | Memory: 45.7 MB (15.14%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,31 @@
 class Solution {
-    private void solve(int idx,int sum,int[] candidates,List<List<Integer>> ans,List<Integer> nums){
+    private void solve(List<Integer> v,int i,int sum,List<Integer> list,List<List<Integer>> ans){
         if(sum==0){
-            ans.add(new ArrayList<>(nums));
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if(sum<0 || idx==candidates.length) return;
+        if(i==v.size() || sum<0) return;
 
-        nums.add(candidates[idx]);
-        solve(idx+1,sum-candidates[idx],candidates,ans,nums);
+        list.add(v.get(i));
+        solve(v,i+1,sum-v.get(i),list,ans);
 
-        nums.remove(nums.size()-1);
-        
-        for(int i=idx+1;i<candidates.length;i++){
-            if(candidates[idx] !=candidates[i]){
-                solve(i,sum,candidates,ans,nums);
+        list.remove(list.size()-1);
+
+        for(int idx=i+1;idx<v.size();idx++){
+            if(v.get(idx)!=v.get(i)){
+                solve(v,idx,sum,list,ans);
                 break;
             }
         }
+
+
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(candidates);
-        solve(0,target,candidates,ans,new ArrayList<>());
+        List<Integer> v=new ArrayList<>();
+        for(int x:candidates) v.add(x);
+        solve(v,0,target,new ArrayList<>(),ans);
         return ans;
     }
 }

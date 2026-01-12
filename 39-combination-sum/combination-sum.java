b79adfe265679b25636da73724465b0e72e2fee3
kasyapgovindu <tinkusai92@gmail.com>
2026-01-12 18:55:36 +0530

    Time: 2 ms (95.18%) | Memory: 45.8 MB (38.85%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,22 @@
 class Solution {
-    private void solve(List<Integer> v,int i,int sum,List<Integer> v2,List<List<Integer>> ans){
+    private void solve(List<Integer> v,int i,int sum,List<Integer> list,List<List<Integer>> ans){
         if(sum==0){
-            ans.add(new ArrayList<>(v2));
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if(sum<0 || i<0) return;
-        solve(v,i-1,sum,v2,ans);
-        v2.add(v.get(i));
-        solve(v,i,sum-v.get(i),v2,ans);
-        v2.remove(v2.size()-1);
+        if(sum<0|| i<0){
+            return;
+        }
+        list.add(v.get(i));
+        solve(v,i,sum-v.get(i),list,ans); // pick
+        list.remove(list.size()-1);
+        solve(v,i-1,sum,list,ans);//not pick
+        
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> v=new ArrayList<>();
-        for(int num:candidates){
-            v.add(num);
-        }
+        for(int x:candidates) v.add(x);
         solve(v,v.size()-1,target,new ArrayList<>(),ans);
         return ans;
     }

a4d96d0fbabaeb09c7476e3649ccb063f057859e
kasyapgovindu <tinkusai92@gmail.com>
2026-01-22 22:54:07 +0530

    Time: 12 ms (92.49%) | Memory: 46.4 MB (55.71%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,14 @@
 class Solution {
-    private int func(int i,int sum,int[] coins,int[][] dp){
-        if(i==0){
-            if(sum%coins[0]==0) return sum/coins[0];
-            else return (int) 1e9;
-        }
-        if(dp[i][sum] != -1) return dp[i][sum];
-
-        int notpick=0+func(i-1,sum,coins,dp);
-        int pick=Integer.MAX_VALUE;
-
-        if(coins[i]<=sum){
-            pick=1+func(i,sum-coins[i],coins,dp);
-        }
-        return dp[i][sum]=Math.min(notpick,pick);
-    }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int coin:coins){
+            for(int j=coin;j<=amount;j++){
+                dp[j]=Math.min(dp[j],dp[j-coin]+1);
+            }
         }
-        int ans=func(n-1,amount,coins,dp);
-        if(ans==(int) 1e9) return -1;
-        return ans;
+        return dp[amount]==amount+1? -1:dp[amount];
     }
 }

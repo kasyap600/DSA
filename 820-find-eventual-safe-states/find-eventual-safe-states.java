0faf23c75593b181da709e987428e4d74d55d981
KasyapGovindu <173028103+kasyap600@users.noreply.github.com>
2026-02-19 11:50:36 +0530

    Time: 4 ms (99.59%) | Memory: 64.1 MB (21.31%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,32 +1,32 @@
 class Solution {
-    private boolean dfs(int node,int[][] graph,boolean[] vis,boolean[] pathvis,boolean[] check) {
+    private boolean dfs(int[][] graph,int node,boolean[] vis,boolean[] pathvis,boolean[] check){
         vis[node]=true;
         pathvis[node]=true;
-        check[node]=false;
+      
         for(int it:graph[node]){
             if(!vis[it]){
-                if(dfs(it,graph,vis,pathvis,check)) return true;
+                if(dfs(graph,it,vis,pathvis,check)) return true;
             }
             else if(pathvis[it]) return true;
         }
         check[node]=true;
         pathvis[node]=false;
         return false;
+
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> path=new ArrayList<>();
         int n=graph.length;
         boolean[] vis=new boolean[n];
         boolean[] pathvis=new boolean[n];
         boolean[] check=new boolean[n];
+
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,pathvis,check);
-            }
+            if(!vis[i]) dfs(graph,i,vis,pathvis,check);
         }
-        List<Integer> temp=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(check[i]) temp.add(i);
+            if(check[i]) path.add(i);
         }
-        return temp;
+        return path;
     }
 }

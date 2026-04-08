b84609389c4df1835b102bb16a4ca834a71ca426
KasyapGovindu <173028103+kasyap600@users.noreply.github.com>
2026-04-09 01:51:17 +0530

    Time: 34 ms (14.91%) | Memory: 47.6 MB (9.14%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,14 +1,21 @@
 class Solution {
+    private int solve(int[] coins,int tar,int[][] dp,int i){
+        if(tar==0) return 0;
+        if(i>=coins.length) return (int)1e9;
+        if(dp[i][tar]!=-1) return dp[i][tar];
+        int not=solve(coins,tar,dp,i+1);
+        int pick=(int) 1e9;
+        if(coins[i]<=tar){
+            pick=1+solve(coins,tar-coins[i],dp,i);
+        }
+        return dp[i][tar]=Math.min(pick,not);
+    }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0]=0;
-        for(int coin:coins){
-            for(int j=coin;j<=amount;j++){
-                dp[j]=Math.min(dp[j],dp[j-coin]+1);
-            }
-        }
-        return dp[amount]==amount+1? -1:dp[amount];
+        int[][] dp=new int[n][amount+1];
+        for(int[] r:dp) Arrays.fill(r,-1);
+         
+        int ans= solve(coins,amount,dp,0);
+        return ans>=(int) 1e9 ? -1 :ans;
     }
 }
